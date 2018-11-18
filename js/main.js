580474599aa49d2767ecd23c988c222de314213e
Ocaich1 <44949602+Ocaich1@users.noreply.github.com>
2018-11-18 14:28:17 -0500

    uploaded javascript file

---
+++ js/main.js
@@ -0,0 +1,39 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+  
+
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+  // Inline popups
+  $('#inline-popups').magnificPopup({
+    delegate: 'a.light-box-popup',
+    removalDelay: 500, //delay removal by X to allow out-animation
+    callbacks: {
+      beforeOpen: function() {
+         this.st.mainClass = this.st.el.attr('data-effect');
+      }
+    },
+    midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+  });
+
+  // Select Picker
+  $('.selectpicker').selectpicker();
+
+})(jQuery); // End of use strict
+
