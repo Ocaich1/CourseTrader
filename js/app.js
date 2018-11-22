3417acabcd5c4e1ee5f6e85a6549b6c5f7eaf911
Ocaich1 <44949602+Ocaich1@users.noreply.github.com>
2018-11-21 21:11:53 -0500

    updated shopping cart

---
+++ js/app.js
@@ -0,0 +1,132 @@
+/*jslint browser: true, devel: true, maxlen: 100, todo: true*/
+
+//create variables
+var html,
+    addCartButton = document.getElementById("pr-btn"),
+    addCartButton1 = document.getElementById("pr-btn1"),
+    addCartButton2 = document.getElementById("pr-btn2"),
+    addCartButton3 = document.getElementById("pr-btn3"),
+    addCartButton4 = document.getElementById("pr-btn4"),
+    addCartButton5 = document.getElementById("pr-btn5"),
+    addCartButton6 = document.getElementById("pr-btn6"),
+    addCartButton7 = document.getElementById("pr-btn7"),
+    addCartButton8 = document.getElementById("pr-btn8"),
+    addCartButton9 = document.getElementById("pr-btn9"),
+    addCartButton10 = document.getElementById("pr-btn10"),
+    addCartButton11 = document.getElementById("pr-btn11"),
+    addCartButton12 = document.getElementById("pr-btn12"),
+    addCartButton13 = document.getElementById("pr-btn13"),
+    addCartButton14 = document.getElementById("pr-btn14"),
+    emptyCartBox = document.getElementById('empty-cart-body'),
+    fullCartBody = document.getElementById("full-cart-body");
+
+//create functions
+function addToCart() {
+    $('#product0').show();
+    //hide empty-cart-body
+    emptyCartBox.style.display = "none";
+
+}
+function addToCart1() {
+    $('#product1').show();
+    //hide empty-cart-body
+    emptyCartBox.style.display = "none";
+
+}
+function addToCart2() {
+    $('#product2').show();
+    //hide empty-cart-body
+    emptyCartBox.style.display = "none";
+
+}
+function addToCart3() {
+    $('#product3').show();
+    //hide empty-cart-body
+    emptyCartBox.style.display = "none";
+
+}
+function addToCart4() {
+    $('#product4').show();
+    //hide empty-cart-body
+    emptyCartBox.style.display = "none";
+
+}
+function addToCart5() {
+    $('#product5').show();
+    //hide empty-cart-body
+    emptyCartBox.style.display = "none";
+
+}
+function addToCart6() {
+    $('#product6').show();
+    //hide empty-cart-body
+    emptyCartBox.style.display = "none";
+
+}
+function addToCart7() {
+    $('#product7').show();
+    //hide empty-cart-body
+    emptyCartBox.style.display = "none";
+
+}
+function addToCart8() {
+    $('#product8').show();
+    //hide empty-cart-body
+    emptyCartBox.style.display = "none";
+
+}
+function addToCart9() {
+    $('#product9').show();
+    //hide empty-cart-body
+    emptyCartBox.style.display = "none";
+
+}
+function addToCart10() {
+    $('#product10').show();
+    //hide empty-cart-body
+    emptyCartBox.style.display = "none";
+
+}
+function addToCart11() {
+    $('#product11').show();
+    //hide empty-cart-body
+    emptyCartBox.style.display = "none";
+
+}
+function addToCart12() {
+    $('#product12').show();
+    //hide empty-cart-body
+    emptyCartBox.style.display = "none";
+
+}
+function addToCart13() {
+    $('#product13').show();
+    //hide empty-cart-body
+    emptyCartBox.style.display = "none";
+
+}
+function addToCart14() {
+    $('#product114').show();
+    //hide empty-cart-body
+    emptyCartBox.style.display = "none";
+
+}
+
+
+//initialized button with addToCart function
+addCartButton.addEventListener('click', addToCart, false);
+addCartButton1.addEventListener('click', addToCart1, false);
+addCartButton2.addEventListener('click', addToCart2, false);
+addCartButton3.addEventListener('click', addToCart3, false);
+addCartButton4.addEventListener('click', addToCart4, false);
+addCartButton5.addEventListener('click', addToCart5, false);
+addCartButton6.addEventListener('click', addToCart6, false);
+addCartButton7.addEventListener('click', addToCart7, false);
+addCartButton8.addEventListener('click', addToCart8, false);
+addCartButton9.addEventListener('click', addToCart9, false);
+addCartButton10.addEventListener('click', addToCart10, false);
+addCartButton11.addEventListener('click', addToCart11, false);
+addCartButton12.addEventListener('click', addToCart12, false);
+addCartButton13.addEventListener('click', addToCart13, false);
+addCartButton14.addEventListener('click', addToCart14, false);
+
