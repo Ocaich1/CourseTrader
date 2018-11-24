099f54051a5ee023c82c36ea1dcc785c7e033d09
Ocaich1 <44949602+Ocaich1@users.noreply.github.com>
2018-11-23 20:42:50 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,131 +1,223 @@
-/*jslint browser: true, devel: true, maxlen: 100, todo: true*/
+/*jslint browser, es6, single, for, devel, this */
 
 //create variables
-var html,
-    addCartButton = document.getElementById("pr-btn"),
-    addCartButton1 = document.getElementById("pr-btn1"),
-    addCartButton2 = document.getElementById("pr-btn2"),
-    addCartButton3 = document.getElementById("pr-btn3"),
-    addCartButton4 = document.getElementById("pr-btn4"),
-    addCartButton5 = document.getElementById("pr-btn5"),
-    addCartButton6 = document.getElementById("pr-btn6"),
-    addCartButton7 = document.getElementById("pr-btn7"),
-    addCartButton8 = document.getElementById("pr-btn8"),
-    addCartButton9 = document.getElementById("pr-btn9"),
-    addCartButton10 = document.getElementById("pr-btn10"),
-    addCartButton11 = document.getElementById("pr-btn11"),
-    addCartButton12 = document.getElementById("pr-btn12"),
-    addCartButton13 = document.getElementById("pr-btn13"),
-    addCartButton14 = document.getElementById("pr-btn14"),
+var //addToCartButtons = document.getElementsByClassName("my-cart-btn"),
+    button1 = document.getElementById("1"),
+    button2 = document.getElementById("2"),
+    button3 = document.getElementById("3"),
+    button4 = document.getElementById("4"),
+    button5 = document.getElementById("5"),
+    button6 = document.getElementById("6"),
+    button7 = document.getElementById("7"),
+    button8 = document.getElementById("8"),
+    button9 = document.getElementById("9"),
+    button10 = document.getElementById("10"),
+    button11 = document.getElementById("11"),
+    button12 = document.getElementById("12"),
+    button13 = document.getElementById("13"),
+    button14 = document.getElementById("14"),
+    button15 = document.getElementById("15"),
     emptyCartBox = document.getElementById('empty-cart-body'),
-    fullCartBody = document.getElementById("full-cart-body");
+    emptyCartBoxFull = true,
+    emptyCartButton = document.getElementById('empty-cart-button'),
+    builtCart = document.getElementById("products-in-cart"),
+    cartQuantity = document.getElementById("offcanvas-cart-number"),
+
+    // Fake JSON data array here should be API call
+    products = [
+        {
+            id: 1,
+            name: "book1",
+            description: "Kogi skateboard tattooed, whatever portland fingerstache coloring book mlkshk leggings flannel dreamcatcher.",
+            imageUrl: "../img/product/small/1.jpg",
+            price: 50,
+            productNum: "product-popup-1"
+    },
+        {
+            id: 2,
+            name: "book2",
+            description: "Kogi skateboard tattooed, whatever portland fingerstache coloring book mlkshk leggings flannel dreamcatcher.",
+            imageUrl: "../img/product/small/2.jpg",
+            price: 50,
+            productNum: "product-popup-2"
+    },
+        {
+            id: 3,
+            name: "book3",
+            description: "Kogi skateboard tattooed, whatever portland fingerstache coloring book mlkshk leggings flannel dreamcatcher.",
+            imageUrl: "../img/product/small/3.jpg",
+            price: 50,
+            productNum: "product-popup-3"
+    },
+        {
+            id: 4,
+            name: "book4",
+            description: "Kogi skateboard tattooed, whatever portland fingerstache coloring book mlkshk leggings flannel dreamcatcher.",
+            imageUrl: "../img/product/small/4.jpg",
+            price: 50,
+            productNum: "product-popup-4"
+    },
+        {
+            id: 5,
+            name: "book5",
+            description: "Kogi skateboard tattooed, whatever portland fingerstache coloring book mlkshk leggings flannel dreamcatcher.",
+            imageUrl: "../img/product/small/5.jpg",
+            price: 50,
+            productNum: "product-popup-5"
+    },
+        {
+            id: 6,
+            name: "book6",
+            description: "Kogi skateboard tattooed, whatever portland fingerstache coloring book mlkshk leggings flannel dreamcatcher.",
+            imageUrl: "../img/product/small/6.jpg",
+            price: 50,
+            productNum: "product-popup-6"
+    },
+        {
+            id: 7,
+            name: "book7",
+            description: "Kogi skateboard tattooed, whatever portland fingerstache coloring book mlkshk leggings flannel dreamcatcher.",
+            imageUrl: "../img/product/small/7.jpg",
+            price: 50,
+            productNum: "product-popup-7"
+    },
+        {
+            id: 8,
+            name: "book8",
+            description: "Kogi skateboard tattooed, whatever portland fingerstache coloring book mlkshk leggings flannel dreamcatcher.",
+            imageUrl: "../img/product/small/8.jpg",
+            price: 50,
+            productNum: "product-popup-8"
+    },
+        {
+            id: 9,
+            name: "book9",
+            description: "Kogi skateboard tattooed, whatever portland fingerstache coloring book mlkshk leggings flannel dreamcatcher.",
+            imageUrl: "../img/product/small/9.jpg",
+            price: 50,
+            productNum: "product-popup-9"
+    },
+        {
+            id: 10,
+            name: "book10",
+            description: "Kogi skateboard tattooed, whatever portland fingerstache coloring book mlkshk leggings flannel dreamcatcher.",
+            imageUrl: "../img/product/small/10.jpg",
+            price: 50,
+            productNum: "product-popup-10"
+    },
+        {
+            id: 11,
+            name: "book11",
+            description: "Kogi skateboard tattooed, whatever portland fingerstache coloring book mlkshk leggings flannel dreamcatcher.",
+            imageUrl: "../img/product/small/11.jpg",
+            price: 50,
+            productNum: "product-popup-11"
+    },
+        {
+            id: 12,
+            name: "book12",
+            description: "Kogi skateboard tattooed, whatever portland fingerstache coloring book mlkshk leggings flannel dreamcatcher.",
+            imageUrl: "../img/product/small/12.jpg",
+            price: 50
+    },
+        {
+            id: 13,
+            name: "book13",
+            description: "Kogi skateboard tattooed, whatever portland fingerstache coloring book mlkshk leggings flannel dreamcatcher.",
+            imageUrl: "../img/product/small/13.jpg",
+            price: 50
+    },
+        {
+            id: 14,
+            name: "book14",
+            description: "Kogi skateboard tattooed, whatever portland fingerstache coloring book mlkshk leggings flannel dreamcatcher.",
+            imageUrl: "../img/product/small/14.jpg",
+            price: 50
+    },
+        {
+            id: 15,
+            name: "book15",
+            description: "Kogi skateboard tattooed, whatever portland fingerstache coloring book mlkshk leggings flannel dreamcatcher.",
+            imageUrl: "../img/product/small/15.jpg",
+            price: 50
+    }
+  ],
+    productsInCart = [];
 
 //create functions
-function addToCart() {
-    $('#product1').show();
-    //hide empty-cart-body
-    emptyCartBox.style.display = "none";
+// This function checks if project is already in productsInCart array
+function productFound(productId) {
+    'use strict';
 
+    return productsInCart.find(function (item) {
+        return item.product.id === productId;
+    });
 }
-function addToCart1() {
-    $('#product2').show();
-    //hide empty-cart-body
-    emptyCartBox.style.display = "none";
 
+function calculateTotalPrice() {
+    'use strict';
+    return productsInCart.reduce(function (total, item) {
+        return total + (item.product.price * item.quantity);
+    }, 0);
 }
-function addToCart2() {
-    $('#product3').show();
-    //hide empty-cart-body
-    emptyCartBox.style.display = "none";
 
+//generates product for basket
+function generateProduct() {
+    'use strict';
+
+    var product = document.createElement("div");
+
+    product.className = "cart-product-list alert";
+    product.role = "alert";
+    product.innerHTML = '<div class="row no-gutters"><div class="col-md-3"><img class="img-fluid" src="img/product/small/3.jpg" alt=""></div><!-- /.col-md-4 --><div class="col-md-9"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button><h2>Portable Bluetooth Speaker</h2><div class="row no-gutters cpl-footer"><div class="col-md-8"><p class="pricing"><strong>$25.00</strong></p></div><div class="col-md-4"><p class="qty">QTY: 1</p></div></div></div><!-- /.col-md-8 --></div><!-- /.row -->';
+
+    console.log(products);
+    builtCart.appendChild(product);
 }
-function addToCart3() {
-    $('#product4').show();
-    //hide empty-cart-body
-    emptyCartBox.style.display = "none";
 
-}
-function addToCart4() {
-    $('#product5').show();
-    //hide empty-cart-body
-    emptyCartBox.style.display = "none";
+// Adds new items or updates existing one in productsInCart array
+function addToCart(id) {
+    'use strict';
 
-}
-function addToCart5() {
-    $('#product6').show();
-    //hide empty-cart-body
-    emptyCartBox.style.display = "none";
+    var obj = products[id];
 
-}
-function addToCart6() {
-    $('#product7').show();
-    //hide empty-cart-body
-    emptyCartBox.style.display = "none";
+    if (productsInCart.length === 0 || productFound(obj.id) === undefined) {
+        productsInCart.push({
+            product: obj,
+            quantity: 1
+        });
+        emptyCartBox.style.display = "none";
+        emptyCartBoxFull = false;
+        generateProduct(obj);
 
-}
-function addToCart7() {
-    $('#product8').show();
-    //hide empty-cart-body
-    emptyCartBox.style.display = "none";
-
-}
-function addToCart8() {
-    $('#product9').show();
-    //hide empty-cart-body
-    emptyCartBox.style.display = "none";
-
-}
-function addToCart9() {
-    $('#product10').show();
-    //hide empty-cart-body
-    emptyCartBox.style.display = "none";
-
-}
-function addToCart10() {
-    $('#product11').show();
-    //hide empty-cart-body
-    emptyCartBox.style.display = "none";
-
-}
-function addToCart11() {
-    $('#product12').show();
-    //hide empty-cart-body
-    emptyCartBox.style.display = "none";
-
-}
-function addToCart12() {
-    $('#product13').show();
-    //hide empty-cart-body
-    emptyCartBox.style.display = "none";
-
-}
-function addToCart13() {
-    $('#product14').show();
-    //hide empty-cart-body
-    emptyCartBox.style.display = "none";
-
-}
-function addToCart14() {
-    $('#product15').show();
-    //hide empty-cart-body
-    emptyCartBox.style.display = "none";
-
+    } else {
+        productsInCart.forEach(function (item) {
+            if (item.product.id === obj.id && productsInCart.length === 0) {
+                item.quantity++;
+            }
+        });
+    }
 }
 
 
-//initialized button with addToCart function
-addCartButton.addEventListener('click', addToCart, false);
-addCartButton1.addEventListener('click', addToCart1, false);
-addCartButton2.addEventListener('click', addToCart2, false);
-addCartButton3.addEventListener('click', addToCart3, false);
-addCartButton4.addEventListener('click', addToCart4, false);
-addCartButton5.addEventListener('click', addToCart5, false);
-addCartButton6.addEventListener('click', addToCart6, false);
-addCartButton7.addEventListener('click', addToCart7, false);
-addCartButton8.addEventListener('click', addToCart8, false);
-addCartButton9.addEventListener('click', addToCart9, false);
-addCartButton10.addEventListener('click', addToCart10, false);
-addCartButton11.addEventListener('click', addToCart11, false);
-addCartButton12.addEventListener('click', addToCart12, false);
-addCartButton13.addEventListener('click', addToCart13, false);
-addCartButton14.addEventListener('click', addToCart14, false);
+//function to empty out cart
+function emptyOutCart() {
+    'use strict';
+
+    builtCart.innerHTML = "";
+    productsInCart = [];
+}
+
+//add event listeners to addToCart buttons
+function addListeners() {
+    'use strict';
+
+    button1.addEventListener('click', addToCart, false);
+    button2.addEventListener('click', addToCart, false);
+    button3.addEventListener('click', addToCart, false);
+    button4.addEventListener('click', addToCart, false);
+    emptyCartButton.addEventListener('click', emptyOutCart, false);
+}
+
+//use function
+addListeners();
